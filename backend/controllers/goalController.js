223f58c273f95ac3e34d3c04715555afe8d48579
Jonathan Tan <jtsz5057@gmail.com>
2023-12-19 14:01:06 +1000

    refractoring by creating controller for the CRUD function

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,35 @@
+// @desc    Get Goals
+// @route   GET /api/goals
+// @access  Private
+const getGoals = (req, res) => {
+    res.status(200).json({ message: 'Get Goals'})
+} 
+
+// @desc    Post Goals
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+    res.status(200).json({ message: 'Set Goal'})
+} 
+
+// @desc    Update Goals
+// @route   PUT /api/goals
+// @access  Private
+const updateGoal = (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` })
+} 
+
+// @desc    Delete Goals
+// @route   DELETE /api/goals
+// @access  Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `Remove goal ${req.params.id}` })
+} 
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,24 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
-})
+router.get('/', getGoals)
 
+router.post('/', setGoal)
 
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set goals' })
-})
+router.put('/:id', updateGoal)
 
+router.delete('/:id', deleteGoal)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update goals ${req.params.id}` })
-})
-
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Remove goals ${req.params.id}` })
-})
+// router.route('/').get(getGoals).post(setGoal)
+// router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 module.exports = router
 
