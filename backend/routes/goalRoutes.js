f4342fd79ebdf6f798280a8724e57365082ca0e4
Jonathan Tan <jtsz5057@gmail.com>
2023-12-19 13:05:01 +1000

    set up routes path for all the CRUD operation

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.status(200).json({ message: 'Get goals' })
+})
+
+
+router.post('/', (req, res) => {
+    res.status(200).json({ message: 'Set goals' })
+})
+
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({ message: `Update goals ${req.params.id}` })
+})
+
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ message: `Remove goals ${req.params.id}` })
+})
+
+module.exports = router
+
